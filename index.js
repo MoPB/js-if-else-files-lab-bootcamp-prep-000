386dbb62708ee50610d0db7018bdd3182ffda674
Bertha Powell <mopowellb@gmail.com>
2018-01-27 00:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ if (name=== "Susan")
 {
     console.log("The
     name is correct")
-}
-else {
-  console.log("Expected
-  the name to be Susan")
-  }
+} else {
 
-Don't worry about this
+  console.log("Expected
+  the name to be Susan"
+  Don't worry about this
 module.exports = { name, height
 }
 
